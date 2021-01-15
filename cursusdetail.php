af5358386906f98dfa39dcfc23b0476e621336fb
Menno Brands | Fontys <menno.brands@student.fontys.nl>
2021-01-15 20:41:49 +0100

    Added a Index and Nav bar
    Added a index.html were you can choose between the two prototypes. Also added the NAV bar with full css and hovers.
    
    Also changed some file to PHP from HTML.

---
+++ cursusdetail.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="height=device-height, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <script src="script/main.js"></script>
+    <title>Geronimo</title>
+</head>
+<body>
+<header>
+    <?php
+    require_once('includes/nav.php');
+    ?>
+</header>
+</body>
+</html>
---
+++ includes/nav.php
@@ -0,0 +1,14 @@
+<div class="NavBar">
+    <div class="navLogo">
+        <a href="index.html"><img class="navLogoLogo" alt="Geronimo Navigatio Logo" src="images/GeronimoLogo.PNG"></a>
+    </div>
+    <div class="navLi">
+        <ul>
+            <li><a href="cursusoverzicht.php">Hobbycursussen |</a></li>
+            <li><a href="gemeenschappen.php">Gemeenschappen |</a></li>
+            <li><a href="#">Groepen |</a></li>
+            <li><a href="#">Contact |</a></li>
+            <li><a href="#">Inloggen</a></li>
+        </ul>
+    </div>
+</div>
